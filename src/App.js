6d1d364958cd6b0bbe32e6e24884707b76ae0504
aletilalitha <aletilalitha@gmail.com>
2022-10-12 20:46:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import SearchWeather from "./components/SearchWeather";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ marginTop: "40px" }}>Weather App</h1>
+      <SearchWeather />
+    </div>
+  );
+}
+
+export default App;
